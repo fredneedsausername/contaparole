bc9115e36d33cc62f93b51d786ff18bce6ae8c66
fredneedsausername <155904115+fredneedsausername@users.noreply.github.com>
2024-06-23 12:54:59 +0200

    Update contaparole.h

--- contaparole.h
+++ contaparole.h
@@ -1,3 +1,4 @@
+#pragma once
 
 #include "fredalberobinario.h"
 #include <stdlib.h>
@@ -7,21 +8,22 @@
 #define MAX_WORD_SIZE 127
 #define MAX_FILE_NAME_LENGTH 50
 
+FILE* output;
 
 typedef struct {
-    char * parola;
+    char* parola;
     int numOccorrenze;
 } contaparole_Parola;
 
-contaparole_Parola * contaparole_crea_parola(char * parola, int numOccorrenze);
-int contaparole_funzione_comparazione_parola(const void * parola1, const void * parolaNodo2);
-int contaparole_funzione_comparazione_struct(const void * parolaNodo1, const void * parolaNodo2);
-int contaparole_funzione_distruzione_contenuto(void * contenuto);
-char * contaparole_ottieni_prossima_parola(FILE * input);
-int contaparole_conta_parola(alberobinario_NodoBinario * root, char * contenuto, alberobinario_pFunzioneComparazione pFComp);
-void contaparole_aggiungi_parola(alberobinario_NodoBinario * root, char * parola);
-void contaparole_funzione_stampa(const void * contenuto);
-void contaparole_inizializza_albero(alberobinario_NodoBinario * root, const char * parola);
+contaparole_Parola* contaparole_crea_parola(char* parola, int numOccorrenze);
+int contaparole_funzione_comparazione_parola(const void* parola1, const void* parolaNodo2);
+int contaparole_funzione_comparazione_struct(const void* parolaNodo1, const void* parolaNodo2);
+int contaparole_funzione_distruzione_contenuto(void* contenuto);
+char* contaparole_ottieni_prossima_parola(FILE* input);
+int contaparole_conta_parola(alberobinario_NodoBinario* root, char* parola);
+void contaparole_aggiungi_parola(alberobinario_NodoBinario* root, char* parola);
+void contaparole_funzione_stampa(const void* contenuto);
+void contaparole_inizializza_albero(alberobinario_NodoBinario* root, const char* parola);
 void contaparole_loop_applicazione();
 
 /* DOCUMENTAZIONE aggiornare return values
@@ -45,7 +47,7 @@ void contaparole_loop_applicazione();
     ritorna stringa vuota se ha incontrato un simbolo. Ogni singola volta che incontra un simbolo ritorna una stringa vuota.
     altrimenti ritorna la prossima parola nel file.
 
-// int contaparole_conta_parola(alberobinario_NodoBinario * root, char * contenuto, alberobinario_pFunzioneComparazione pFComp)
+// int contaparole_conta_parola(alberobinario_NodoBinario * root, char * parola)
     funzione per ricavare il conteggio del numero di apparizioni di una parola.
     ritorna numOccorrenze se la parola è trovata.
     ritorna -1 se non è trovata.
@@ -54,7 +56,7 @@ void contaparole_loop_applicazione();
     aggiunge una parola all'albero binario, se è già presente aggiunge 1 al numero di apparizioni di quella parola.
 
 // void contaparole_funzione_stampa(const void * contenuto);
-    stampa tutte le parole lette con relativo conteggio di frequenza.
+    stampa tutte le parole lette al file output con relativo conteggio di frequenza.
 
 // void contaparole_loop_applicazione()
     chiamare questa funzione per l'applicazione da terminale implementata con questa libreria.
